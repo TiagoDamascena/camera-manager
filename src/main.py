45e7f07df07c77fc5b8a9d6a4009067f7c561124
Renato Leal <marcondesleal@icloud.com>
2023-11-10 18:08:44 -0300

    Capture

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+import os
 import gphoto2 as gp
 camera = gp.Camera()
 camera.init()
 text = camera.get_summary()
-print('Summary')
-print('=======')
-print(str(text))
+print(text)
+file_path = camera.capture(gp.GP_CAPTURE_IMAGE)
+print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+target = os.path.join('/Users/renatoleal/Desktop', file_path.name)
+camera_file = camera.file_get(file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
+camera_file.save(target)
 camera.exit()
